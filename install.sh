02045d98dc86409693e5164c3a9162c332761b83
kongjiadongyuan <zhaggbl@foxmail.com>
2022-04-24 04:39:04 +0000

    add self route support

--- install.sh
+++ install.sh
@@ -9,6 +9,11 @@ fi
 
 docker run -v $SERVER_VOLUME_NAME:/etc/openvpn --rm kylemanna/openvpn ovpn_genconfig -u $CONNECT_METHOD://$SERVER_ADDR:$CONNECT_PORT -d -D -b -c -s $VPN_SUBNET
 docker run -v $SERVER_VOLUME_NAME:/etc/openvpn --rm -it kylemanna/openvpn ovpn_initpki
-docker run -v $SERVER_VOLUME_NAME:/etc/openvpn --name openvpn_server -d -p $CONNECT_PORT:1194/$CONNECT_METHOD --cap-add=NET_ADMIN kylemanna/openvpn
+docker run -v $SERVER_VOLUME_NAME:/etc/openvpn --name $SERVER_DOCKER_NAME --restart=always -d -p $CONNECT_PORT:1194/$CONNECT_METHOD --cap-add=NET_ADMIN kylemanna/openvpn
+
+# edit ovpn_run
+docker exec $SERVER_DOCKER_NAME sed -i "2i /usr/local/bin/iptables.sh" /usr/local/bin/ovpn_run
+echo "#!/bin/sh" | docker exec -i $SERVER_DOCKER_NAME tee -a /usr/local/bin/iptables.sh
+docker exec $SERVER_DOCKER_NAME chmod +x /usr/local/bin/iptables.sh
 
 python3 refreshconfig.py
--- refreshconfig.py
+++ refreshconfig.py
@@ -20,17 +20,28 @@ while True:
     else:
         break
 route_list = {}
+self_route = []
 for item in content:
-    tmpitem = item.split()
-    nw = ipaddress.ip_network(tmpitem[1])
-    hostname = tmpitem[0]
-    if not hostname in route_list.keys():
-        route_list[hostname] = []
+    if item.startswith('#'):
+        continue
+    try:
+        tmpitem = item.split()
+        print(item)
+        nw = ipaddress.ip_network(tmpitem[1])
+        hostname = tmpitem[0]
+    except Exception as e:
+        print(e)
+        continue
     toinsert = {}
     toinsert['subnet'] = str(nw.network_address)
     toinsert['prefix_length'] = str(nw.prefixlen)
     toinsert['netmask'] = str(nw.netmask)
-    route_list[hostname].append(toinsert)
+    if hostname == '_self':
+        self_route.append(toinsert)   
+    else:
+        if not hostname in route_list.keys():
+            route_list[hostname] = []
+        route_list[hostname].append(toinsert)
 
 # generate openvpn.conf
 cmd = f"docker run -v {SERVER_VOLUME_NAME}:/etc/openvpn -it --rm kylemanna/openvpn ovpn_genconfig -u {CONNECT_METHOD}://{SERVER_ADDR}:{CONNECT_PORT} -d -D -b -c -s {VPN_SUBNET}"
@@ -38,17 +49,29 @@ for hostname in route_list.keys():
     for item in route_list[hostname]:
         cmd += f''' -r "{item['subnet']}/{item['prefix_length']}" '''
         cmd += f''' -p "route {item['subnet']} {item['netmask']}" '''
+for item in self_route:
+    cmd += f''' -p "route {item['subnet']} {item['netmask']}" '''
+
 print(cmd)
 os.system(cmd)
 
 # generate /etc/openvpn/ccd/*
 
-os.system(f"""docker run -v {SERVER_VOLUME_NAME}:/etc/openvpn -it --rm kylemanna/openvpn sh -c "rm /etc/openvpn/ccd/*" """)
+os.system(f""" docker run -v {SERVER_VOLUME_NAME}:/etc/openvpn -it --rm kylemanna/openvpn sh -c "rm /etc/openvpn/ccd/*" """)
 for hostname in route_list.keys():
     toecho = ''
     for item in route_list[hostname]:
         toecho += f"iroute {item['subnet']} {item['netmask']}\n"
     cmd = f""" echo '{toecho}' | docker run -v {SERVER_VOLUME_NAME}:/etc/openvpn -i --rm kylemanna/openvpn tee /etc/openvpn/ccd/{hostname} """
+    os.system(cmd)
+
+# prepare iptables
+toecho = '#!/bin/sh'
+cmd = f""" echo '{toecho}' | docker exec -i {SERVER_DOCKER_NAME} tee /usr/local/bin/iptables.sh """
+os.system(cmd)
+for item in self_route:
+    toecho = f'''iptables -t nat -A POSTROUTING -d {item['subnet']}/{item['prefix_length']} -o eth0 -j MASQUERADE'''
+    cmd = f""" echo '{toecho}' | docker exec -i {SERVER_DOCKER_NAME} tee -a /usr/local/bin/iptables.sh """
     os.system(cmd)
 
 # restart docker
